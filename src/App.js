b8110401e712ec40cb222a6fd6ef2ab789d0ff08
simonbandmann <s.bandmann@gmail.com>
2021-10-01 14:36:15 +0200

    Deleted some text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Just a short test.</p>
-
-        <p>And another test.</p>
       </header>
     </div>
   );
